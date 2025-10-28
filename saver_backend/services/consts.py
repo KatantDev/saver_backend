1c0ae4c4106a48897cd61e0aa6cd617553008a6b
Katant <katantdev@mail.ru>
2025-10-29 00:03:59 +1000

    feat: base download path

--- saver_backend/services/consts.py
+++ saver_backend/services/consts.py
@@ -1,5 +1,4 @@
 from pathlib import Path
 
-BASE_DOWNLOAD_PATH = Path("downloads")
-
+BASE_DOWNLOAD_PATH = Path(__file__).resolve().parent / "downloads"
 MAX_FILE_SIZE_BYTES = 3 * 1024 * 1024 * 1024
