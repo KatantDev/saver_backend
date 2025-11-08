59e2123a97879ed14fd44367ec08bfa5eb685c4b
Katant <katantdev@mail.ru>
2025-11-08 23:48:57 +1000

    feat: custom source_id and title for m3u8 controller

--- saver_backend/services/downloaders/m3u8_ydl_source.py
+++ saver_backend/services/downloaders/m3u8_ydl_source.py
@@ -1,4 +1,5 @@
 from typing import Any, ClassVar
+from urllib.parse import urlparse
 
 from saver_backend.entities.enums import SourceEnum
 from saver_backend.services.downloaders.ydl_source import YtDlpController
@@ -19,3 +20,28 @@ class M3U8YdlController(YtDlpController):
             "downloader_args": ["-x", "16", "-s", "16", "-k", "1M"],
         }
         self._yt_dlp.params.update(params)
+
+    async def get_video_info(self, url: str) -> dict[str, Any] | None:
+        """
+        Get video info from m3u8 url.
+
+        :param url: The url to get video info from.
+        :return: The video info or None.
+        """
+        result = await super().get_video_info(url=url)
+        url_path = urlparse(self._resolution.url).path[:200]
+        if result is None or self._video is None:
+            return None
+
+        self._video = self._video.model_copy(
+            update={"title": url_path, "source_id": url_path},
+        )
+        result.update(
+            {
+                "id": self._video.source_id,
+                "title": self._video.title,
+                "fulltitle": self._video.title,
+            },
+        )
+
+        return result
