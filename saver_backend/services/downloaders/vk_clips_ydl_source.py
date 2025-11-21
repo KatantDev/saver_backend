24a91618b8232811330b9b837a8482651249da41
Katant <katantdev@mail.ru>
2025-11-22 00:06:31 +1000

    feat: disable cookies for vk

--- saver_backend/services/downloaders/vk_clips_ydl_source.py
+++ saver_backend/services/downloaders/vk_clips_ydl_source.py
@@ -9,7 +9,7 @@ class VKClipsYdlController(YtDlpController):
 
     SOURCE: ClassVar[SourceEnum] = SourceEnum.VK_CLIPS_YDL
     PROXY_TYPE: ClassVar[ProxyType] = ProxyType.RU
-    COOKIES: ClassVar[bool] = True
+    COOKIES: ClassVar[bool] = False
     DIRECT_URL_DOWNLOAD: ClassVar[bool] = False
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
