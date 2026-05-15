ba4a966b58db04a09b05dcf5f5d130f0c0aefad7
Alexandr Kuryanov <univerxy@sbomail.ru>
2026-05-15 10:12:47 +0300

    settings: CDP port 9222 → 9223 ffmpgeg.run() → to_thread fix double model validate, comment for fsm_dto[-1] added YandexMusicEnum.CODECSEPARATOR instead '-' fix "High Quality [Lossless]" to _("High Quality [Lossless]")

--- saver_backend/entities/enums.py
+++ saver_backend/entities/enums.py
@@ -64,6 +64,12 @@ class YandexMusicContentTypeEnum(str, Enum):
     ALBUM = "album"
 
 
+class YandexMusicEnum(str, Enum):
+    """Enum for Yandex Music."""
+
+    CODECSEPARATOR = "-"
+
+
 class VideoTheatreEnum(str, Enum):
     """Enum for Video Theatre."""
 
