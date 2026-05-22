098c08dad45d4ea52da8809a0806ffa2d96de7ef
Alexandr Kuryanov <univerxy@sbomail.ru>
2026-05-22 15:15:13 +0300

    changed the method of getting a site mirror

--- saver_backend/settings.py
+++ saver_backend/settings.py
@@ -86,7 +86,8 @@ class Settings(BaseSettings):
     # chrome headless settings
     taskiq_worker_host: str = "saver_backend-taskiq-worker"
     chrome_host: str = "saver_backend-chrome"
-    chrome_port: int = 9223
+    # chrome container make port forwarding from 9222 to 9223
+    chrome_port: int = 9222
     _chrome_cdp_url: Optional[str] = None
 
     # expired dto timeout in hours
